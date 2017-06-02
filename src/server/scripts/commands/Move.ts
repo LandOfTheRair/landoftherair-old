a924f45e0dd6dbb0f989853a134ca1fcfc14a87a
Kyle Kemp <kyle@seiyria.com>
2017-06-02 09:22:09 -0500

    density works

--- src/server/scripts/commands/Move.ts
+++ src/server/scripts/commands/Move.ts
@@ -7,11 +7,80 @@ export class Move extends Command {
   public name = '~move';
 
   execute(client, player: Player, { map, x, y }) {
-    x = Math.min(x, player.stats.move);
-    y = Math.min(y, player.stats.move);
+    x = Math.max(Math.min(x, player.stats.move), -player.stats.move);
+    y = Math.max(Math.min(y, player.stats.move), -player.stats.move);
 
-    player.x += x;
-    player.y += y;
+    const checkX = Math.abs(x);
+    const checkY = Math.abs(y);
+
+    const denseTiles = map.layers[4].data;
+    const maxTilesMoved = Math.max(checkX, checkY);
+    const steps = Array(maxTilesMoved).fill(null);
+
+    let tempX = x;
+    let tempY = y;
+
+    for(let curStep = 0; curStep < maxTilesMoved; curStep++) {
+
+      const step = { x: 0, y: 0 };
+
+      if(tempX < 0) {
+        step.x = -1;
+        tempX++;
+      } else if(tempX > 0) {
+        step.x = 1;
+        tempX--;
+      }
+
+      if(tempY < 0) {
+        step.y = -1;
+        tempY++;
+      } else if(tempY > 0) {
+        step.y = 1;
+        tempY--;
+      }
+
+      steps[curStep] = step;
+    }
+
+    const reverseSteps = steps.reverse();
+
+    const getNumStepsSuccessful = (trySteps) => {
+      for(var i = 0; i < trySteps.length; i++) {
+        const step = trySteps[i];
+        const nextTileLoc = ((player.y + step.y) * map.width) + (player.x + step.x);
+        const nextTile = denseTiles[nextTileLoc];
+
+        if(nextTile !== 0) {
+          break;
+        }
+      }
+
+      return i;
+    };
+
+    const normalStepsTaken = getNumStepsSuccessful(steps);
+    const reverseStepsTaken = getNumStepsSuccessful(reverseSteps);
+
+    const finalSteps = normalStepsTaken > reverseStepsTaken ? steps : reverseSteps;
+
+    finalSteps.forEach(step => {
+      const nextTileLoc = ((player.y + step.y) * map.width) + (player.x + step.x);
+      const nextTile = denseTiles[nextTileLoc];
+
+      if(nextTile !== 0) {
+       return;
+      }
+
+      player.x += step.x;
+      player.y += step.y;
+    });
+
+    // console.log(steps);
+
+
+    // player.x += x;
+    // player.y += y;
 
     if(player.x < 0) player.x = 0;
     if(player.y < 0) player.y = 0;
@@ -19,9 +88,6 @@ export class Move extends Command {
     if(player.x > map.width)  player.x = map.width;
     if(player.y > map.height) player.y = map.height;
 
-    const checkX = Math.abs(x);
-    const checkY = Math.abs(y);
-    
     if(checkX >= checkY) {
       if(x > 0) {
         player.dir = 'E';
