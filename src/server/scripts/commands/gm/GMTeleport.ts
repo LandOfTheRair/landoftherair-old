51940fe23622c9a0b23f19030b6ead28dd29af8a
Kyle Kemp <kyle@seiyria.com>
2017-06-05 16:53:41 -0500

    gm teleport for easy travel

---
+++ src/server/scripts/commands/gm/GMTeleport.ts
@@ -0,0 +1,23 @@
+
+import { Command } from '../../../base/Command';
+import { Player } from '../../../../models/player';
+
+export class GMTeleport extends Command {
+
+  public name = '@teleport';
+  public format = 'X Y [Map]';
+
+  execute(player: Player, { gameState, args }) {
+    if(!player.isGM) return;
+
+    const [x, y, map] = args.split(' ');
+
+    // TODO implement map teleporting
+
+    player.x = +x;
+    player.y = +y;
+
+    gameState.calculateFOV(player);
+  }
+
+}
