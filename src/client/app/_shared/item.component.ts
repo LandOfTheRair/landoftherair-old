8609925ccf695936dcb232cdcbd7a5ec62b3c932
Kyle Kemp <kyle@seiyria.com>
2017-06-07 18:42:04 -0500

    tooltip on dblclick for items desc

--- src/client/app/_shared/item.component.ts
+++ src/client/app/_shared/item.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 
 import { environment } from '../../environments/environment';
 import { Item } from '../../../models/item';
+import { Player } from '../../../models/player';
 
 @Component({
   selector: 'app-item',
@@ -27,9 +28,9 @@ import { Item } from '../../../models/item';
     }
   `],
   template: `
-    <div class="item-container">
+    <div class="item-container" [isDisabled]="!showDesc" triggers="dblclick:mouseleave" [tooltip]="descText">
       <img [src]="imgUrl" [style.object-position]="spriteLocation" />
-      <span class="count" *ngIf="realCount > 0">{{ realCount }}</span>
+      <span class="count" *ngIf="!imageOnly && realCount > 0">{{ realCount }}</span>
     </div>
   `
 })
@@ -40,6 +41,15 @@ export class ItemComponent {
 
   @Input()
   public count: number;
+
+  @Input()
+  public imageOnly: boolean;
+
+  @Input()
+  public showDesc: boolean;
+
+  @Input()
+  public player: Player;
 
   get realCount() {
     if(this.item.itemClass === 'Coin') return this.item.value;
@@ -54,6 +64,11 @@ export class ItemComponent {
     const y = Math.floor(this.item.sprite/32);
     const x = this.item.sprite%32;
     return `-${x*64}px -${y*64}px`;
+  }
+
+  get descText() {
+    const item = new Item(this.item);
+    return item.descTextFor(this.player);
   }
 
   // TODO draggable, highlight appropriate containers with red border if not usable and green border if usable
