029601e4645216d46b9882a0c6b058c2cfbff156
Kyle Kemp <kyle@seiyria.com>
2017-06-07 19:00:36 -0500

    item condition indicators

--- src/client/app/_shared/item.component.ts
+++ src/client/app/_shared/item.component.ts
@@ -26,11 +26,32 @@ import { Player } from '../../../models/player';
       right: 5px;
       font-size: 0.7rem;
     }
+    
+    .glow-container {
+      border-radius: 32px;
+      height: 64px;
+      width: 64px;
+      position: absolute;
+      top: 0;
+    }
+    
+    .glow-yellow {
+      box-shadow: inset 0 0 14px #f60;
+    }
+    
+    .glow-red {
+      box-shadow: inset 0 0 14px #f00;
+    }
+    
+    .glow-black {
+      box-shadow: inset 0 0 14px #000;
+    }
   `],
   template: `
     <div class="item-container" [isDisabled]="!showDesc" triggers="dblclick:mouseleave" [tooltip]="descText">
       <img [src]="imgUrl" [style.object-position]="spriteLocation" />
-      <span class="count" *ngIf="!imageOnly && realCount > 0">{{ realCount }}</span>
+      <div class="glow-container {{ glowColor }}" *ngIf="showDesc"></div>
+      <span class="count" *ngIf="realCount > 0">{{ realCount }}</span>
     </div>
   `
 })
@@ -41,9 +62,6 @@ export class ItemComponent {
 
   @Input()
   public count: number;
-
-  @Input()
-  public imageOnly: boolean;
 
   @Input()
   public showDesc: boolean;
@@ -58,6 +76,13 @@ export class ItemComponent {
 
   get imgUrl() {
     return `${environment.client.protocol}://${environment.client.domain}:${environment.client.port}/assets/items.png`;
+  }
+
+  get glowColor() {
+    if(this.item.condition <= 0)     return 'glow-black';
+    if(this.item.condition <= 5000)  return 'glow-red';
+    if(this.item.condition <= 10000) return 'glow-yellow';
+    return '';
   }
 
   get spriteLocation() {
