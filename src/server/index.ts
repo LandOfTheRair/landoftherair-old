23cee76c09cb52e2557076f234a8600e1519b5fb
Kyle Kemp <kyle@seiyria.com>
2017-06-07 19:27:10 -0500

    add dotenv

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,8 @@
 import { DB } from './database';
 import { API } from './api';
 
+require('dotenv').config({ silent: true });
+
 if(!process.env.AUTH0_SECRET) {
   console.log('No env.AUTH0_SECRET. Set one.');
   process.exit(0);
