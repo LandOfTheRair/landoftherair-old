4fd98f02a0c8392eb58fd7613da8d5a769ad97a5
Kyle Kemp <kyle@seiyria.com>
2017-06-07 19:50:07 -0500

    fix dotenv
    Former-commit-id: 84eeac15e9ef4b0cbdcfc6fc903807793a5ac766

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,8 @@
 
+require('dotenv').config({ silent: true });
+
 import { DB } from './database';
 import { API } from './api';
-
-require('dotenv').config({ silent: true });
 
 if(!process.env.AUTH0_SECRET) {
   console.log('No env.AUTH0_SECRET. Set one.');
