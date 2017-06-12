638b126d3a88620ab346c8f8ac2b0f57f97b8e1a
Kyle Kemp <kyle@seiyria.com>
2017-06-12 08:42:45 -0500

    set up npcs with color tags
    Former-commit-id: 393dbefe3679895cec4050b8cef26d302dd974fa

--- src/server/scripts/npc/tutorial/hermit.ts
+++ src/server/scripts/npc/tutorial/hermit.ts
@@ -1,7 +1,14 @@
 import { NPC } from '../../../../models/npc';
+import { NPCLoader } from '../../../helpers/npc-loader';
 
-export const setup = (npc: NPC) => {
+export const setup = async (npc: NPC) => {
   npc.hostility = 'Never';
+
+  npc.leftHand = await NPCLoader.loadItem('Tutorial Yeti Skull');
+  npc.rightHand = await NPCLoader.loadItem('Tutorial Key');
+  npc.gear.Armor = await NPCLoader.loadItem('Antanian Tunic');
+
+  npc.recalculateStats();
 };
 
 export const responses = (npc: NPC) => {
