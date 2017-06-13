30ca0c327dd62d29c7dc694abd61474cd178ef3f
Kyle Kemp <kyle@seiyria.com>
2017-06-13 10:11:40 -0500

    npcs spawn with gear
    Former-commit-id: 242fadfd69258621c73f403e54a55c67e81399f5

---
+++ src/server/base/spawner.ts
@@ -0,0 +1,131 @@
+
+import { NPCLoader } from '../helpers/npc-loader';
+
+import { sample, random, extend } from 'lodash';
+import { NPC } from '../../models/npc';
+import { Logger } from '../logger';
+
+export class Spawner {
+
+  x: number;
+  y: number;
+  map: number;
+
+  currentTick = 0;
+
+  // in ticks
+  respawnRate = 60;
+
+  initialSpawn = 0;
+
+  maxCreatures = 5;
+
+  // TODO -1 to spawn anywhere in map
+  spawnRadius = 0;
+
+  randomWalkRadius = 10;
+
+  // TODO -1 to disable
+  leashRadius = 20;
+
+  paths: string[];
+
+  npcIds: string[];
+
+  npcAISettings: string[];
+
+  npcs: NPC[] = [];
+
+  constructor(private room, { x, y, map }, spawnOpts) {
+    extend(this, spawnOpts);
+
+    this.x = x / 64;
+    this.y = y / 64;
+    this.map = map;
+
+    for(let i = 0; i < this.initialSpawn; i++) {
+      this.createNPC();
+    }
+  }
+
+  isActive() {
+    return true;
+  }
+
+  private async chooseItemFrom(choices: string[]) {
+    const item = sample(choices);
+    if(item) return NPCLoader.loadItem(item);
+    return null;
+  }
+
+  private chooseSpriteFrom(choices: number[]): number {
+    if(!choices || choices.length === 0) return 0;
+    return sample(choices);
+  }
+
+  async createNPC() {
+    if(!this.npcIds || this.npcIds.length === 0) {
+      Logger.error(`No valid npcIds for spawner ${this.constructor.name} at ${this.x}, ${this.y} on ${this.map}`);
+      return;
+    }
+
+    const npcData = await NPCLoader.loadNPCData(sample(this.npcIds));
+    if(!npcData) {
+      Logger.error(`No valid spawn for spawner ${this.constructor.name} at ${this.x}, ${this.y} on ${this.map}`);
+      return;
+    }
+
+    npcData.sprite = this.chooseSpriteFrom(npcData.sprite);
+
+    npcData.rightHand = await this.chooseItemFrom(npcData.rightHand);
+    npcData.leftHand = await this.chooseItemFrom(npcData.leftHand);
+
+    await Promise.all(Object.keys(npcData.gear).map(async slot => {
+      const item = await this.chooseItemFrom(npcData.gear[slot]);
+      npcData.gear[slot] = item;
+      return item;
+    }));
+
+    npcData.x = random(this.x - this.spawnRadius, this.x + this.spawnRadius);
+    npcData.y = random(this.y - this.spawnRadius, this.y + this.spawnRadius);
+    npcData.map = this.map;
+
+    const ai = sample(this.npcAISettings) || 'default';
+    npcData.ai = require(`../scripts/ai/${ai}`);
+
+    if(!npcData.name) {
+      npcData.name = this.room.determineNPCName(npcData);
+    }
+
+    const npc = new NPC(npcData);
+
+    npc.spawner = this;
+    this.assignPath(npc);
+    this.addNPC(npc);
+    npc.recalculateStats();
+
+    return npc;
+  }
+
+  addNPC(npc: NPC) {
+    this.npcs.push(npc);
+    this.room.state.addNPC(npc);
+  }
+
+  assignPath(npc: NPC) {
+    if(!this.paths || this.paths.length === 0) return false;
+    npc.path = sample(this.paths);
+  }
+
+  tick() {
+    if(!this.isActive) return;
+
+    this.currentTick++;
+    if(this.currentTick > this.respawnRate && this.npcs.length < this.maxCreatures) {
+      this.createNPC();
+    }
+
+    this.npcs.forEach(npc => npc.tick());
+  }
+
+}
--- src/server/helpers/npc-loader.ts
+++ src/server/helpers/npc-loader.ts
@@ -1,8 +1,16 @@
 
 import { ItemCreator } from './item-creator';
 import { NPC } from '../../models/npc';
+import { DB } from '../database';
 
 export class NPCLoader {
+
+  static loadNPCData(npcId) {
+    return DB.$npcs.findOne({ npcId }).then(npc => {
+      if(!npc) throw new Error(`NPC ${npcId} does not exist.`);
+      return npc;
+    });
+  }
 
   static loadItem(item) {
     return ItemCreator.getItemByName(item);
