91e8f3069cb47822791c4425eccc8cb0c6631a4c
Kyle Kemp <kyle@seiyria.com>
2017-06-14 14:15:18 -0500

    scaffold becoming a class
    Former-commit-id: ac4a8f6a3e81a6772ef172bdcb1558a72814ac3c

---
+++ src/server/classes/Mage.ts
@@ -0,0 +1,9 @@
+
+import { BaseClass } from '../base/BaseClass';
+import { Character } from '../../models/character';
+
+export class Mage extends BaseClass {
+  static becomeClass(character: Character) {
+
+  }
+}
---
+++ src/server/classes/Thief.ts
@@ -0,0 +1,9 @@
+
+import { BaseClass } from '../base/BaseClass';
+import { Character } from '../../models/character';
+
+export class Thief extends BaseClass {
+  static becomeClass(character: Character) {
+
+  }
+}
