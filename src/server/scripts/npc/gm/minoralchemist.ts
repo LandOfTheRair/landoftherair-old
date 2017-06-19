4493df110e8066863a43e0cb7b0b3c1498420052
Kyle Kemp <kyle@seiyria.com>
2017-06-19 13:52:01 -0500

    add GMCastle
    Former-commit-id: 2bc7acaaba7d4b3bb4609cab7802277125bc97ee

---
+++ src/server/scripts/npc/gm/minoralchemist.ts
@@ -0,0 +1,31 @@
+import { NPC } from '../../../../models/npc';
+import { NPCLoader } from '../../../helpers/npc-loader';
+import { VendorResponses } from '../common-responses';
+
+export const setup = async (npc: NPC) => {
+  npc.hostility = 'Never';
+
+  const vendorItems = [
+    'Antanian Strength Potion',
+    'Antanian Dexterity Potion',
+    'Antanian Agility Potion',
+    'Antanian Intelligence Potion',
+    'Antanian Wisdom Potion',
+    'Antanian Willpower Potion',
+    'Antanian Constitution Potion',
+    'Antanian Charisma Potion',
+    'Antanian Luck Potion',
+    'Antanian Health Potion',
+    'Antanian Magic Potion'
+  ];
+
+  
+  NPCLoader.loadVendorItems(npc, vendorItems);
+
+  npc.gear.Armor = await NPCLoader.loadItem('Battlemage Armor');
+  npc.recalculateStats();
+};
+
+export const responses = (npc: NPC) => {
+  VendorResponses(npc);
+};
