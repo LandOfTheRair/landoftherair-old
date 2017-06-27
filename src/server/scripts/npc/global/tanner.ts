53146f556984a5fc6f7e383f782a7a0eb9b91a5f
Kyle Kemp <kyle@seiyria.com>
2017-06-27 18:49:06 -0500

    tanner npc, closes #31
    Former-commit-id: cb7342e3855804149645e020f7bdfb4fb7b0e443 [formerly 3e56a22832e9756fd3e1525c7608b839c6a50ce0]
    Former-commit-id: be08f99996b56a064b18361ba578dbbebf4e8a71

---
+++ src/server/scripts/npc/global/tanner.ts
@@ -0,0 +1,22 @@
+import { NPC } from '../../../../models/npc';
+import { NPCLoader } from '../../../helpers/npc-loader';
+import { RandomlyShouts, TannerResponses } from '../common-responses';
+
+export const setup = async (npc: NPC) => {
+  npc.hostility = 'Never';
+
+  npc.gear.Armor = await NPCLoader.loadItem('Antanian Tunic');
+  npc.recalculateStats();
+};
+
+export const responses = (npc: NPC) => {
+
+  TannerResponses(npc);
+  RandomlyShouts(npc, [
+    'Best furs made from your kills!',
+    'Bring your kills, leave with furs!',
+    'You kill them, we clean them!',
+    'Most fur is better than a tunic!'
+  ]);
+
+};
