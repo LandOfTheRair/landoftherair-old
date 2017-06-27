d7e167ef493f4e32b7d339b31c987df8526b68b8
Kyle Kemp <kyle@seiyria.com>
2017-06-27 13:19:16 -0500

    smith npc. closes #79
    Former-commit-id: 8c5c996b9b96f3ca4f8ed11f34df7f28a689b90c

---
+++ src/server/scripts/npc/antania/rylt/smith.ts
@@ -0,0 +1,15 @@
+import { NPC } from '../../../../../models/npc';
+import { NPCLoader } from '../../../../helpers/npc-loader';
+import { SmithResponses } from '../../common-responses';
+
+export const setup = async (npc: NPC) => {
+  npc.hostility = 'Never';
+
+  npc.rightHand = await NPCLoader.loadItem('Smith Hammer');
+  npc.gear.Armor = await NPCLoader.loadItem('Antanian Breastplate');
+  npc.recalculateStats();
+};
+
+export const responses = (npc: NPC) => {
+  SmithResponses(npc);
+};
