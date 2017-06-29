ab1938cc84aed36dc3262e078468a7bebeb80883
Kyle Kemp <kyle@seiyria.com>
2017-06-29 11:33:00 -0500

    renegade sidequest
    Former-commit-id: 02d109921dac7208c6e655ab02b1d75184718e34 [formerly a9e7dc03028caf23b56660780def9ead726b30d0]
    Former-commit-id: e7cd2df3334613fe3dec7c344d13524937db56e6

---
+++ src/server/scripts/npc/antania/rylt/renegade.ts
@@ -0,0 +1,38 @@
+import { NPC } from '../../../../../models/npc';
+import { NPCLoader } from '../../../../helpers/npc-loader';
+import { SmithResponses } from '../../common-responses';
+
+const RENEGADE_BOOK = 'Rylt Renegade Book';
+
+export const setup = async (npc: NPC) => {
+  npc.hostility = 'Never';
+
+  npc.rightHand = await NPCLoader.loadItem(RENEGADE_BOOK);
+  npc.gear.Armor = await NPCLoader.loadItem('Antanian Tunic');
+  npc.recalculateStats();
+};
+
+export const responses = (npc: NPC) => {
+  npc.parser.addCommand('hello')
+    .set('syntax', ['hello'])
+    .set('logic', (args, { player }) => {
+      if(NPCLoader.checkPlayerHeldItem(player, RENEGADE_BOOK)) {
+        NPCLoader.takePlayerItem(player, RENEGADE_BOOK);
+
+        NPCLoader.loadItem('Antanian Health Potion')
+          .then(item => {
+            player.setRightHand(item);
+          });
+
+        return 'Well, thank you, kind adventurer. Here is your reward, as promised.';
+      }
+
+      return `Why, hello. It's not often that I get visitors down here in my hidey-hole. I like to read, they call me the read-agade. Get it? Anyway, if you can bring me some BOOKS to stave off the boredom of my already-vast collection, I'd be grateful. Those muscleheads above us have some novels I've not yet read. They're too dimwitted to understand the intricacies of the words on the page, so I'd appreciate them being given to someone who does. Me, that is. Be warned though, those barbarians rip apart books on the regular.`;
+    });
+
+  npc.parser.addCommand('books')
+    .set('syntax', ['books'])
+    .set('logic', (args, { player }) => {
+      return `Books, books. Ah yes. Rewards, you want one, yes? If you bring me a book and hold it in your right hand, I'll give you a potion to permanently increase your health. It's very handy. Be warned that it does have limitations.`;
+    });
+};
