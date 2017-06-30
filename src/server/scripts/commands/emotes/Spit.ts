156aea432be5d3375453a619086a7fb2279e890e
Kyle Kemp <kyle@seiyria.com>
2017-06-30 09:02:24 -0500

    an emote works,
    Former-commit-id: 12007c1855ca6ac4764df47c9ca99ff63988a2e4 [formerly 4437b1a7a7401f92f1c0db9e2438bbb4289fcd20]
    Former-commit-id: 354cbfe37ed52362375812972442aad8a61f3dc5

---
+++ src/server/scripts/commands/emotes/Spit.ts
@@ -0,0 +1,27 @@
+
+import { Command } from '../../../base/Command';
+import { Player } from '../../../../models/player';
+
+export class Spit extends Command {
+
+  public name = 'spit';
+  public format = 'Target?';
+
+  async execute(player: Player, { room, gameState, args }) {
+    const possTargets = room.getPossibleMessageTargets(player, args);
+
+    if(possTargets && possTargets[0]) {
+      const target = possTargets[0];
+
+      player.sendClientMessage(`You spit at ${target.name}!`);
+      target.sendClientMessage(`${player.name} spits at you!`);
+      player.sendClientMessageToRadius(`${player.name} spits at ${target.name}!`, 4, [player.uuid, target.uuid]);
+      target.addAgro(player, 1);
+      return;
+    }
+
+    player.sendClientMessage('You spit!');
+    player.sendClientMessageToRadius(`${player.name} spits!`, 4, [player.uuid]);
+    
+  }
+}
--- src/server/scripts/commands/index.ts
+++ src/server/scripts/commands/index.ts
@@ -1,6 +1,7 @@
 
 export * from './basic';
 export * from './debug';
+export * from './emotes';
 export * from './gm';
 export * from './internal';
 export * from './skills';
