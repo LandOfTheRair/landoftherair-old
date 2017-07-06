1488fc7efa96c2259a45386666e8732206fd07df
seiyria <kyle@seiyria.com>
2017-07-06 12:00:00 -0500

    npc scripts

---
+++ src/server/scripts/spawners/antania/rylt-prison-rebel.ts
@@ -0,0 +1,24 @@
+
+import { Spawner } from '../../../base/Spawner';
+
+const npcIds = [
+  'Rylt Renegade Rebel',
+  'Rylt Renegade Mage Rebel',
+  'Rylt Renegade Healer Rebel'
+];
+
+export class RyltPrisonRebelSpawner extends Spawner {
+
+  constructor(room, opts) {
+    super(room, opts, {
+      respawnRate: 20,
+      initialSpawn: 1,
+      maxCreatures: 5,
+      spawnRadius: 0,
+      randomWalkRadius: 15,
+      leashRadius: 25,
+      npcIds
+    });
+  }
+
+}
