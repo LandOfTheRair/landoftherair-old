00a337f676c20e16d15fe4d1e5390e194ed26317
Kyle Kemp <kyle@seiyria.com>
2017-07-08 18:16:41 -0500

    add ~CtS, closes #93

---
+++ src/server/scripts/commands/internal/coin/coin-to-sack.ts
@@ -0,0 +1,17 @@
+
+import { find, isUndefined } from 'lodash';
+
+import { Command } from '../../../../base/Command';
+import { Player } from '../../../../../models/player';
+import { ItemCreator } from '../../../../helpers/item-creator';
+
+export class CoinToSack extends Command {
+
+  public name = '~CtS';
+  public format = '';
+
+  async execute(player: Player, { room, gameState, args }) {
+    // it is a no-op
+  }
+
+}
