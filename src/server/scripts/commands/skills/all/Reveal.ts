d89a91124070fc31911e5e76f2792b07df868dcc
Kyle Kemp <kyle@seiyria.com>
2017-10-06 16:19:54 -0500

    Add reveal, #123
    Former-commit-id: 1bda98b6c4b5bf6dc01e7f92bdba8b70f816f7a8 [formerly b2413a7616f2d3532c9b2cedf2eb4b69b31a93ac]
    Former-commit-id: 1a102841b4be760173c723d998d9abb9714cb491

---
+++ src/server/scripts/commands/skills/all/Reveal.ts
@@ -0,0 +1,24 @@
+
+import { startsWith } from 'lodash';
+
+import { Skill } from '../../../../base/Skill';
+import { Character } from '../../../../../models/character';
+
+export class Reveal extends Skill {
+
+  public name = 'reveal';
+  public format = '';
+
+  requiresLearn = false;
+
+  execute(user: Character, { gameState }) {
+    this.use(user);
+  }
+
+  use(user: Character) {
+    const hidden = user.hasEffect('Hidden');
+    if(!hidden) return user.sendClientMessage('You are not hidden!');
+    user.unapplyEffect(hidden, true);
+  }
+
+}
