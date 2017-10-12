732ce622caff1670f6a4aecb56c54e72fadaa8a4
Kyle Kemp <kyle@seiyria.com>
2017-10-12 12:23:01 -0500

    remove dist

--- dist/server/rooms/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./Lobby"));
-__export(require("./GameWorld"));
-//# sourceMappingURL=index.js.map
--- dist/server/scripts/commands/skills/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./all"));
-__export(require("./monster"));
-__export(require("./spells"));
-//# sourceMappingURL=index.js.map
