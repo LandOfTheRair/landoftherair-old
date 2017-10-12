e58543e6e6e3acb15ad8bbcc3ba2c66a0f62c4c7
Kyle Kemp <kyle@seiyria.com>
2017-10-12 07:44:51 -0500

    error handling for scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,34 @@
 var { emptyDirSync } = require('fs-extra');
 var exec = require('child_process').exec;
 
-exec('git add -f dist', () => {
-  exec('git commit -am "evennode dist"', () => {
-    exec('git push evennode master', () => {
-      exec('git reset --hard HEAD~1', () => {
+exec('git add -f dist', (e) => {
+
+  if(e) {
+    console.error(e);
+    process.exit(0);
+  }
+
+  exec('git commit dist/* -m "evennode dist"', (e) => {
+
+    if(e) {
+      console.error(e);
+      process.exit(0);
+    }
+
+    exec('git push evennode master', (e) => {
+
+      if(e) {
+        console.error(e);
+        process.exit(0);
+      }
+
+      exec('git reset --hard HEAD~1', (e) => {
+
+        if(e) {
+          console.error(e);
+          process.exit(0);
+        }
+
         emptyDirSync('dist');
       });
     });
