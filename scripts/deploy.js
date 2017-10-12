ad84b8ac8e7995614997f43253ba424ccc5f6f98
Kyle Kemp <kyle@seiyria.com>
2017-10-12 12:27:27 -0500

    deploy > more verbose

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,31 +2,42 @@
 var { emptyDirSync } = require('fs-extra');
 var exec = require('child_process').exec;
 
-exec('git add -f dist', (e) => {
+exec('git add -f dist', (e, stdout, stderr) => {
+
+  console.log('Added dist files');
 
   if(e) {
+    console.log(stdout, stderr);
     console.error(e);
     process.exit(0);
   }
 
   exec('git commit dist/* -m "evennode dist"', (e, stdout, stderr) => {
 
+    console.log('Committed dist files');
+
     if(e) {
-      console.log(stdout, stderr)
+      console.log(stdout, stderr);
       console.error(e);
       process.exit(0);
     }
 
-    exec('git push -f evennode master', (e) => {
+    exec('git push -f evennode master', (e, stdout, stderr) => {
+
+      console.log('Pushed dist files');
 
       if(e) {
+        console.log(stdout, stderr);
         console.error(e);
         process.exit(0);
       }
 
-      exec('git reset --hard HEAD~1', (e) => {
+      exec('git reset --hard HEAD~1', (e, stdout, stderr) => {
+
+        console.log('Reset dist files');
 
         if(e) {
+          console.log(stdout, stderr);
           console.error(e);
           process.exit(0);
         }
