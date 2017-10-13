0278a75668c6f6c1db00aea3957fb6edd6b62aee
Kyle Kemp <kyle@seiyria.com>
2017-10-13 09:14:17 -0500

    add botanist quest, closes #178
    Former-commit-id: 560bfb8b996f8f8b4532f4cdb80e5be76a03fc70 [formerly 7725f8ca4942c97901fc1623029fa1f2ad69fed3]
    Former-commit-id: cbea3fadb11b4b0e7e349b516d5aa8cb59390254

---
+++ src/server/scripts/npc/antania/rylt/botanist.ts
@@ -0,0 +1,37 @@
+import { NPC } from '../../../../../models/npc';
+import { NPCLoader } from '../../../../helpers/npc-loader';
+
+const FLOWER = 'Rylt Blueflower';
+const QUESTNAME = 'Rylt Botanist Regeneration';
+
+export const setup = async (npc: NPC) => {
+  npc.hostility = 'Never';
+
+  npc.leftHand = await NPCLoader.loadItem(FLOWER);
+  npc.rightHand = await NPCLoader.loadItem(FLOWER);
+  npc.gear.Armor = await NPCLoader.loadItem('Antanian Tunic');
+
+  npc.recalculateStats();
+};
+
+export const responses = (npc: NPC) => {
+
+  npc.parser.addCommand('hello')
+    .set('syntax', ['hello'])
+    .set('logic', (args, { player }) => {
+
+      if(player.hasPermanentCompletionFor(QUESTNAME)) return `You've already brought me flowers, ${player.name}!`;
+
+      if(NPCLoader.checkPlayerHeldItem(player, FLOWER, 'right')
+      && NPCLoader.checkPlayerHeldItem(player, FLOWER, 'left')) {
+        NPCLoader.takePlayerItem(player, FLOWER, 'right');
+        NPCLoader.takePlayerItem(player, FLOWER, 'left');
+        player.gainStat('hpregen', 1);
+        player.gainStat('mpregen', 1);
+        player.permanentlyCompleteQuest(QUESTNAME);
+        return `Ah, thank you ${player.name}! Here, enjoy your increased regeneration.`;
+      }
+
+      return `Well hello there, adventurer! I am a roaming botanist and I seek the flowers of this land. If you can bring me two, I can increase your natural regeneration using a technique I learned from my master!`;
+    });
+};
