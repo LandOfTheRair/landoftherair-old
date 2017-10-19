2bd46b14498a5de24d97d7f3bb1711ec7cb04d7e
Kyle Kemp <kyle@seiyria.com>
2017-10-19 14:32:45 -0500

    add more steffen merchants
    Former-commit-id: d947403364b89ff76a043ad0914b1d9301559a74 [formerly 5402a6c356f541bd96f3e5cf9939d1ad4f515f50]
    Former-commit-id: 37438d5ffd9cf76e5d09f3aefd191aeba326fa97

---
+++ src/server/scripts/npc/antania/yzalt/steffen-armor-vendor.ts
@@ -0,0 +1,27 @@
+import { NPC } from '../../../../../shared/models/npc';
+import { NPCLoader } from '../../../../helpers/npc-loader';
+import { VendorResponses } from '../../common-responses';
+
+export const setup = async (npc: NPC) => {
+  npc.hostility = 'Never';
+
+  const vendorItems = [
+    'Antanian Ringmail Tunic',
+    'Antanian Breastplate',
+    'Antanian Cloak',
+    'Antanian Leather Gloves',
+    'Antanian Leather Boots',
+    'Leather Ring'
+  ];
+
+  NPCLoader.loadVendorItems(npc, vendorItems);
+
+  npc.rightHand = await NPCLoader.loadItem('Antanian Halberd');
+  npc.gear.Armor = await NPCLoader.loadItem('Antanian Breastplate');
+
+  npc.recalculateStats();
+};
+
+export const responses = (npc: NPC) => {
+  VendorResponses(npc);
+};
