a62d9d9e89b2d370985adfdba861dbce618f951c
Kyle Kemp <kyle@seiyria.com>
2017-10-21 21:39:12 -0500

    move psn and disease to shared

--- src/server/scripts/commands/skills/spells/healer/index.ts
+++ src/server/scripts/commands/skills/spells/healer/index.ts
@@ -2,7 +2,5 @@ export * from './Afflict';
 export * from './Antidote';
 export * from './BarNecro';
 export * from './Cure';
-export * from './Disease';
-export * from './Poison';
 export * from './Revive';
 export * from './Succor';
