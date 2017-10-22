dd89d7f2e46ed740a1afa49fd363e6de40ff26ee
Kyle Kemp <kyle@seiyria.com>
2017-10-22 09:44:30 -0500

    drink now takes from sack, closes #201
    Former-commit-id: f76a7157a1f877d5c360de37df7418f346ff19c6 [formerly d91fc67b1b1130fcdce9e6daee910ce5d31099a5]
    Former-commit-id: 3595fb6c0b2080188bf1849b2686994863b94d4b

--- src/server/scripts/commands/internal/Drink.ts
+++ src/server/scripts/commands/internal/Drink.ts
@@ -1,5 +1,5 @@
 
-import { startsWith } from 'lodash';
+import { findLastIndex } from 'lodash';
 
 import { Command } from '../../../base/Command';
 import { Player } from '../../../../shared/models/player';
@@ -18,10 +18,46 @@ export class Drink extends Command {
   public name = '~drink';
 
   execute(player: Player, { room, gameState, args }) {
-    const item = player.potionHand;
-    if(!item) return player.sendClientMessage('You do not have a potion to drink!');
+    // const item = player.potionHand;
+    // if(!item) return player.sendClientMessage('You do not have a potion to drink!');
 
-    player.useItem('potionHand');
+    // if you have a potion, it takes priority, period
+    if(player.potionHand) {
+      player.useItem('potionHand');
+      return;
+    }
+
+    if(player.rightHand && player.rightHand.effect && player.rightHand.effect.name === 'ExactHeal') {
+      player.useItem('rightHand');
+      return;
+    }
+
+    if(player.leftHand && player.leftHand.effect && player.leftHand.effect.name === 'ExactHeal') {
+      player.useItem('leftHand');
+      return;
+    }
+
+    if(!player.hasEmptyHand()) return player.sendClientMessage('Your hands are full!');
+
+    const itemIndex = findLastIndex(player.sack.allItems, (item) => {
+      if(!item.effect) return false;
+      return item.effect.name === 'ExactHeal';
+    });
+
+    if(itemIndex === -1) return player.sendClientMessage('You have no potions to drink!');
+
+    const emptyHand = player.rightHand ? 'Left' : 'Right';
+    const slotName = `${emptyHand.toLowerCase()}Hand`;
+    const func = player[`set${emptyHand}Hand`].bind(player);
+
+    const useItemInHand = (itemSlot) => {
+      player.useItem(itemSlot);
+    };
+
+    const item = player.sack.getItemFromSlot(itemIndex);
+    func(item);
+    player.sack.takeItemFromSlot(itemIndex);
+    useItemInHand(slotName);
   }
 
 }
