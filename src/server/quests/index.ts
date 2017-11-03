c63681e481662ecc9c6f96c1f405f726ab4897ab
Kyle Kemp <kyle@seiyria.com>
2017-11-03 17:39:48 -0500

    fix quest error
    Former-commit-id: f44024274da3d965d24ba0da6a00c67e8cba87b2 [formerly beb5497f9e019e43581c77f90c5ea1ad70672c6c]
    Former-commit-id: 64026e1df04c775e67b378991e089254e275e647

--- src/server/quests/index.ts
+++ src/server/quests/index.ts
@@ -1,5 +1,8 @@
 
 export * from './antania/Rylt/KillRebels';
+
 export * from './antania/Yzalt/KillSteffen';
+export * from './antania/Yzalt/KillHeniz';
 export * from './antania/Yzalt/HenizFindSedgwick';
+export * from './antania/Yzalt/SteffenFindSedgwick';
 export * from './antania/Yzalt/KillRanata';
