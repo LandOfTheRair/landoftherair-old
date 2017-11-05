8222c5e96cc9e4363c799d501314e6560ffbaf0b
Kyle Kemp <kyle@seiyria.com>
2017-11-05 07:58:12 -0600

    closes #253
    Former-commit-id: 6f6791830c1959ec1973aedff5d61aaec0d8e88a [formerly 7ab4c28b7d819bd152f50a3fde0d02a45fa64683]
    Former-commit-id: 4e30630b5cd3959425d4522fac3228b080667c44

---
+++ src/server/scripts/commands/debug/DebugCountMobs.ts
@@ -0,0 +1,13 @@
+
+import { Command } from '../../../base/Command';
+import { Player } from '../../../../shared/models/player';
+
+export class DebugCountMobs extends Command {
+
+  public name = '~~mobs';
+
+  execute(player: Player, { room, args }) {
+    player.sendClientMessage(`Currently ${room.state._mapNPCs.length} NPCs and ${room.state.players.length} players in ${room.state.mapName}.`);
+  }
+
+}
--- src/server/scripts/commands/debug/index.ts
+++ src/server/scripts/commands/debug/index.ts
@@ -1,2 +1,4 @@
+
+export * from './DebugCountMobs';
 export * from './DebugPos';
 export * from './DebugReset';
