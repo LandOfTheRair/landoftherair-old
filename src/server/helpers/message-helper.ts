ccbc2b99f1dd09b93be705e7ce0ff71c287c0465
Kyle Kemp <kyle@seiyria.com>
2017-12-10 16:54:04 -0600

    log messages work again

--- src/server/helpers/message-helper.ts
+++ src/server/helpers/message-helper.ts
@@ -7,13 +7,13 @@ export class MessageHelper {
 
   static sendClientMessage(char: Character, message) {
     if(!char.isPlayer()) return;
-    char.$$room.sendPlayerLogMessage(this, message);
+    char.$$room.sendPlayerLogMessage(char, message);
   }
 
   static sendClientMessageToRadius(char: Character, message, radius = 0, except = []) {
     const sendMessage = isString(message) ? { message, subClass: 'chatter' } : message;
 
-    char.$$room.state.getPlayersInRange(this, radius, except).forEach(p => {
+    char.$$room.state.getPlayersInRange(char, radius, except).forEach(p => {
       // outta range, generate a "you heard X in the Y dir" message
       if(radius > 4 && char.distFrom(p) > 5) {
         const dirFrom = char.getDirBasedOnDiff(char.x - p.x, char.y - p.y);
@@ -26,7 +26,7 @@ export class MessageHelper {
   }
 
   static drawEffectInRadius(char: Character, effectName: VisualEffect, center: any, effectRadius = 0, drawRadius = 0) {
-    char.$$room.state.getPlayersInRange(this, drawRadius).forEach(p => {
+    char.$$room.state.getPlayersInRange(char, drawRadius).forEach(p => {
       p.$$room.drawEffect(p, { x: center.x, y: center.y }, effectName, effectRadius);
     });
   }
