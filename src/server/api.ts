e115e0705324dabfd0022643b8efa324235c8eb0
Kyle Kemp <kyle@seiyria.com>
2017-12-11 10:10:11 -0600

    fork process, closes #269
    Former-commit-id: 6339fb5a1a4c6da784e6a4a42ba39b3c06877330 [formerly f12162b273b64d2bba3fbcdec8d9dcb8209948b5]
    Former-commit-id: b1327fd100822d09c5ad2e9a864709f65b5f1281

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,73 +1,42 @@
 
-import * as colyseus from 'colyseus';
-import * as http from 'http';
 import * as express from 'express';
 import * as cors from 'cors';
 import * as staticFile from 'connect-static-file';
-
-import * as Rooms from './rooms';
-
-import * as recurse from 'recursive-readdir';
-import * as path from 'path';
 import * as processStats from 'process-stats';
 
 import { DB } from './database';
-import { Logger } from './logger';
 
 import { drawNormal as drawNormalArmor } from './tasks/analysis/_armor';
 import { drawNormal as drawNormalWeapon, drawSpecial as drawSpecialWeapon } from './tasks/analysis/_weapons';
 
-class GameAPI {
+export class GameAPI {
 
-  public isReady: Promise<any>;
+  public expressApp: any;
 
   constructor() {
-    this.isReady = new Promise(resolve => {
-      const app = express();
-      app.use(cors());
+    const app = express();
+    app.use(cors());
 
-      app.use('/logs', async (req, res) => {
-        const dbLogs = await DB.$logs.find().toArray();
-        res.json(dbLogs);
-      });
-
-      app.use('/server', (req, res) => {
-        res.json(processStats());
-      });
-
-      app.use('/item-stats', async (req, res) => {
-        const statStrings = await Promise.all([drawNormalArmor(), drawNormalWeapon(), drawSpecialWeapon()]);
-        res.send(statStrings.map(x => `<pre>${x}</pre>`).join(''))
-      });
-
-      app.use('/silent-dev', staticFile(`${__dirname}/silent-dev.html`));
-
-      app.use('/silent-production', staticFile(`${__dirname}/silent-production.html`));
-
-      app.use('/maps', express.static(require('path').join(__dirname, 'maps')));
-
-      const port = process.env.PORT || 3303;
-
-      const server = http.createServer(app);
-      const gameServer = new colyseus.Server({ server });
-
-      gameServer.register('Lobby', Rooms.Lobby);
-
-      const allMapNames = {};
-
-      recurse(`${__dirname}/maps`).then(files => {
-        files.forEach(file => {
-          const mapName = path.basename(file, path.extname(file));
-          allMapNames[mapName] = true;
-          gameServer.register(mapName, Rooms.GameWorld, { mapName, mapPath: file, allMapNames });
-        });
-      });
-
-      server.listen(port);
-      Logger.log(`Started server on port ${port}`);
-      resolve();
+    app.use('/logs', async (req, res) => {
+      const dbLogs = await DB.$logs.find().toArray();
+      res.json(dbLogs);
     });
+
+    app.use('/server', (req, res) => {
+      res.json(processStats());
+    });
+
+    app.use('/item-stats', async (req, res) => {
+      const statStrings = await Promise.all([drawNormalArmor(), drawNormalWeapon(), drawSpecialWeapon()]);
+      res.send(statStrings.map(x => `<pre>${x}</pre>`).join(''))
+    });
+
+    app.use('/silent-dev', staticFile(`${__dirname}/silent-dev.html`));
+
+    app.use('/silent-production', staticFile(`${__dirname}/silent-production.html`));
+
+    app.use('/maps', express.static(require('path').join(__dirname, 'maps')));
+
+    this.expressApp = app;
   }
 }
-
-export const API = new GameAPI();
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,17 +2,21 @@
 require('dotenv').config({ silent: true });
 
 import { DB } from './database';
-import { API } from './api';
+import { GameAPI } from './api';
 import { Logger } from './logger';
+
+import * as colyseus from 'colyseus';
+
+import * as Rooms from './rooms';
+
+import * as recurse from 'recursive-readdir';
+import * as path from 'path';
+import * as cluster from 'cluster';
 
 if(!process.env.AUTH0_SECRET) {
   console.log('No env.AUTH0_SECRET. Set one.');
   process.exit(0);
 }
-
-Promise.all([DB.isReady, API.isReady]).then(() => {
-  console.log('Server ready!');
-});
 
 process.on('unhandledRejection', e => {
   Logger.error(e);
@@ -21,3 +25,35 @@ process.on('unhandledRejection', e => {
 process.on('uncaughtException', e => {
   Logger.error(e);
 });
+
+const port = process.env.PORT || 3303;
+
+const gameServer = new colyseus.ClusterServer({});
+
+if(cluster.isMaster) {
+  gameServer.listen(port);
+  Logger.log(`[Master] Started server on port ${port}`);
+
+  gameServer.fork();
+
+} else {
+  Logger.log(`[Child] Started child process ${process.pid}`);
+
+  const api = new GameAPI();
+
+  gameServer.register('Lobby', Rooms.Lobby);
+
+  const allMapNames = {};
+
+  recurse(`${__dirname}/maps`).then(files => {
+    files.forEach(file => {
+      const mapName = path.basename(file, path.extname(file));
+      allMapNames[mapName] = true;
+      gameServer.register(mapName, Rooms.GameWorld, { mapName, mapPath: file, allMapNames });
+    });
+  });
+
+  DB.isReady.then(() => {
+    gameServer.attach({ server: api.expressApp });
+  });
+}
