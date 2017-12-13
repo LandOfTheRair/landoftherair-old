9bfc03c30504a2c46c129882ad3191cb97c2fcef
Kyle Kemp <kyle@seiyria.com>
2017-12-12 18:24:53 -0600

    update prod var

--- src/client/environments/environment.prod.ts
+++ src/client/environments/environment.prod.ts
@@ -19,6 +19,6 @@ export const environment = {
     apiUrl: 'authidentifier'
   },
   deepstream: {
-    url: 'wss://deepstream.rair.land:6020/deepstream'
+    url: 'wss://deepstream.rair.land'
   }
 };
