97966b3f4b0a47aba05087496881064f413a0feb
Kyle Kemp <kyle@seiyria.com>
2017-12-17 19:04:29 -0600

    add chase command, closes #249
    Former-commit-id: 255c30a4bfe67b4d97905570b4ddc77bed95c6dc [formerly e20d3ea31e1ce487924aa432f4238a24a22327a5]
    Former-commit-id: 69d61ff54c42f281fc9eeeb1d46e39bb9092147d

---
+++ src/server/scripts/commands/basic/Chase.ts
@@ -0,0 +1,44 @@
+
+import { startsWith } from 'lodash';
+
+import { Skill } from '../../../base/Skill';
+import { Character } from '../../../../shared/models/character';
+import { MoveHelper } from '../../../helpers/move-helper';
+
+export class Chase extends Skill {
+
+  static macroMetadata = {
+    name: 'Chase',
+    macro: 'chase',
+    icon: 'hooded-figure',
+    color: '#000000',
+    mode: 'lockActivation',
+    tooltipDesc: 'Move towards a target.'
+  };
+
+  public name = 'chase';
+
+  requiresLearn = false;
+
+  range = (attacker: Character) => 4;
+
+  execute(user: Character, { args }) {
+    if(!args) return false;
+
+    const possTargets = user.$$room.getPossibleMessageTargets(user, args);
+    const target = possTargets[0];
+    if(!target) return user.sendClientMessage('You do not see that person.');
+
+    this.use(user, target);
+  }
+
+  use(user: Character, target: Character) {
+    const xDiff = target.x - user.x;
+    const yDiff = target.y - user.y;
+
+    MoveHelper.move(user, { room: user.$$room, gameState: user.$$room.state, x: xDiff, y: yDiff }, true);
+
+    user.$$room.updatePos(user);
+  }
+
+}
