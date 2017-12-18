95056d26a8e1937a6235f64fc51e3856ec9ebc4c
Kyle Kemp <kyle@seiyria.com>
2017-12-17 19:57:41 -0600

    add tutorial encruster
    Former-commit-id: cb4ec0b8e22da337e7fd10b32b643dcb0bba92ec [formerly 3857c85f7a75d926ecb1493715dc6253fc4f64a5]
    Former-commit-id: e4cbe4cce1ccd3b871e656b7622f5e4fbe98ea8c

---
+++ src/server/scripts/npc/tutorial/encruster.ts
@@ -0,0 +1,29 @@
+import { NPC } from '../../../../shared/models/npc';
+import { NPCLoader } from '../../../helpers/npc-loader';
+
+export const setup = async (npc: NPC) => {
+  npc.hostility = 'Never';
+
+  npc.gear.Armor = await NPCLoader.loadItem('Antanian Tunic');
+  npc.rightHand = await NPCLoader.loadItem('Antanian Shortsword');
+  npc.leftHand = await NPCLoader.loadItem('Antanian Purple Gem');
+
+  npc.rightHand.encrust = {
+    desc: 'a small purple gem',
+    stats: {},
+    sprite: 213,
+    value: 300
+  };
+
+  npc.recalculateStats();
+};
+
+export const responses = (npc: NPC) => {
+
+  npc.parser.addCommand('hello')
+    .set('syntax', ['hello'])
+    .set('logic', (args, { player }) => {
+      return `Hello, ${player.name}! Welcome to Land of the Rair. I am an encruster, and I can do two things for you: make your gear distinguishable, and add stats to your gear! If you hold a gem and item in your hand, I can add various stats to the item! Find my brethren in the game and they can help you!`;
+    });
+
+};
