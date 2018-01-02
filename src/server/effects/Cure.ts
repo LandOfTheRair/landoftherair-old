43b2b6f88d02506d1dcac1923d849a1eaa94402d
Kyle Kemp <kyle@seiyria.com>
2018-01-02 14:53:11 -0600

    closes #344
    Former-commit-id: 2e60ede6deb40793c9797eb4bf4a75cbc11fa27b [formerly a52d5179124159e4a31162d4ae11a44d70944a89]
    Former-commit-id: 2d336a737193dec9877d93699516cc5803702e69

--- src/server/effects/Cure.ts
+++ src/server/effects/Cure.ts
@@ -12,10 +12,10 @@ export class Cure extends SpellEffect {
   cast(caster: Character, target: Character, skillRef?: Skill) {
     this.setPotencyAndGainSkill(caster, skillRef);
 
-    let mult = 0.5;
-    if(this.potency > 0)  mult = 1;
-    if(this.potency > 11) mult = 4;
-    if(this.potency > 21) mult = 8;
+    let mult = 1;
+    if(this.potency > 0)  mult = 2;
+    if(this.potency > 11) mult = 6;
+    if(this.potency > 21) mult = 10;
 
     const wisCheck = Math.floor(mult * this.getCasterStat(caster, 'wis'));
     const damage = -+dice.roll(`${this.potency || 1}d${wisCheck}`);
