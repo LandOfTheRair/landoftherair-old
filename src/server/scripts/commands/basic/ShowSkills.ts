630017d964130b8a1ca89c5739823f41d9367ff1
Kyle Kemp <kyle@seiyria.com>
2018-02-13 12:20:08 -0600

    your allegiance lies with "the none" lol
    Former-commit-id: a3235857b01e9f2deba3dbe9f24805a9dc9ffd3b [formerly 01de1abc29ec0de1258afe1fd7837a63e0347644]
    Former-commit-id: 9ab77abcd11104dbe356a1a614b8f71a753a7b86

--- src/server/scripts/commands/basic/ShowSkills.ts
+++ src/server/scripts/commands/basic/ShowSkills.ts
@@ -17,7 +17,7 @@ export class ShowSkills extends Command {
 
   execute(player: Player, { room, args }) {
     player.sendClientMessage(`You are ${player.name}, the ${player.alignment} level ${player.level} ${player.baseClass}.`);
-    player.sendClientMessage(`Your allegiance lies with the ${player.allegiance}.`);
+    player.sendClientMessage(`Your allegiance lies with ${player.allegiance === 'None' ? 'no one' : `the ${player.allegiance}`}.`);
 
     Object.keys(player.allSkills).forEach(key => {
       player.sendClientMessage(`Your ${key.toUpperCase()} skill level is ${player.calcSkillLevel(key)}.`);
--- src/server/scripts/commands/basic/ShowStats.ts
+++ src/server/scripts/commands/basic/ShowStats.ts
@@ -18,7 +18,7 @@ export class ShowStats extends Command {
 
   execute(player: Player, { room, args }) {
     player.sendClientMessage(`You are ${player.name}, the ${player.alignment} level ${player.level} ${player.baseClass}.`);
-    player.sendClientMessage(`Your allegiance lies with the ${player.allegiance}.`);
+    player.sendClientMessage(`Your allegiance lies with ${player.allegiance === 'None' ? 'no one' : `the ${player.allegiance}`}.`);
 
     Object.keys(player.baseStats).forEach((key: StatName) => {
       player.sendClientMessage(`Your ${key.toUpperCase()} is ${player.getTotalStat(key)} (BASE: ${player.getBaseStat(key)}).`);
