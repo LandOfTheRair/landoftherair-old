79d9595ab9942e0bf14031bcc51704f3a6f082fb
Kyle Kemp <kyle@seiyria.com>
2018-02-18 08:46:48 -0600

    fix pouch loading inconsistencies
    Former-commit-id: 7a8419e161dc204dc368b45565d1e7668542b14c [formerly fa97d68291a8ec9da6426b16e3a36e207aac323b]
    Former-commit-id: bdced073c83be1f719535003d9c805cfbc59235a

--- src/server/helpers/pouch-helper.ts
+++ src/server/helpers/pouch-helper.ts
@@ -6,11 +6,17 @@ import { Player } from '../../shared/models/player';
 import { Pouch } from '../../shared/models/container/pouch';
 
 export class PouchHelper {
-  static savePouch(player: Player) {
-    return DB.$characterPouches.update(
+  static async savePouch(player: Player) {
+    await DB.$characterPouches.update(
       { username: player.username },
       { username: player.username, pouch: player.pouch },
       { upsert: true }
+    );
+
+    return DB.$players.update(
+      { username: player.username },
+      { $set: { pouch: player.pouch } },
+      { multi: true }
     );
   }
 
