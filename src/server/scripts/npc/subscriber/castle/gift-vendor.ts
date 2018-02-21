e9ee35927c9abacf6550dd57d26f10560296bc4a
Kyle Kemp <kyle@seiyria.com>
2018-02-21 13:02:42 -0600

    closes #455

--- src/server/scripts/npc/subscriber/castle/gift-vendor.ts
+++ src/server/scripts/npc/subscriber/castle/gift-vendor.ts
@@ -7,7 +7,12 @@ export const setup = async (npc: NPC) => {
 
   const dailyItems = [
     'Antanian Daily Gem RNG Box',
-    'Antanian Daily Gem RNG Box'
+    'Antanian Daily Gem RNG Box',
+    'Antanian Daily General Rune Scroll RNG Box',
+    'Antanian Daily Healer Rune Scroll RNG Box',
+    'Antanian Daily Mage Rune Scroll RNG Box',
+    'Antanian Daily Thief Rune Scroll RNG Box',
+    'Antanian Daily Warrior Rune Scroll RNG Box'
   ];
 
   await NPCLoader.loadDailyVendorItems(npc, dailyItems);
