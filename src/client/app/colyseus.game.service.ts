68a9e757d5b42547a786ebe17d92efa4b8694faa
Kyle Kemp <kyle@seiyria.com>
2018-03-01 10:31:57 -0600

    lint fix

--- src/client/app/colyseus.game.service.ts
+++ src/client/app/colyseus.game.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NgZone } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { ClientGameState } from './clientgamestate';
 import * as swal from 'sweetalert2';
 
