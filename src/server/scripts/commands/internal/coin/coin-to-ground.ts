db322f74064d1818c55928fed9c7c7a1543f91a8
Kyle Kemp <kyle@seiyria.com>
2018-03-01 07:26:38 -0600

    closes #517

--- src/server/scripts/commands/internal/coin/coin-to-ground.ts
+++ src/server/scripts/commands/internal/coin/coin-to-ground.ts
@@ -11,7 +11,7 @@ export class CoinToGround extends Command {
 
   async execute(player: Player, { room, gameState, args }) {
     const value = +args;
-    if(value <= 0 || value > player.gold || isNaN(value)) return false;
+    if(value <= 0 || value > player.gold || isNaN(value)) return;
     if(!player.hasEmptyHand()) return player.sendClientMessage('Your hands are full.');
 
     const item = await player.$$room.itemCreator.getGold(value);
