8b4c73e356a0ab54928dd4790815b364a18511fb
Kyle Kemp <kyle@seiyria.com>
2018-03-02 18:43:11 -0600

    update maps spritesheets, update risan mines

---
+++ src/server/scripts/npc/risan/mines/crier.ts
@@ -0,0 +1,27 @@
+import { NPC } from '../../../../../shared/models/npc';
+import { NPCLoader } from '../../../../helpers/npc-loader';
+import { RandomlyShouts, CrierResponses } from '../../common-responses';
+
+export const setup = async (npc: NPC) => {
+  npc.hostility = 'Never';
+
+  npc.gear.Armor = await NPCLoader.loadItem('Antanian Tunic');
+  npc.recalculateStats();
+};
+
+export const responses = (npc: NPC) => {
+
+  CrierResponses(npc);
+  RandomlyShouts(npc, [
+    'You should not go far without armor or weapons!',
+    'Potions can be a saving grace!',
+    'The Alchemist can make your potions last longer!',
+    'Thieves prefer to hide!',
+    'The Banker can hold onto your gold!',
+    'The Smith can repair your broken and breaking gear!',
+    'Try asking the Healer to RECALL you!',
+    'The Healer can REVIVE your comrades!',
+    'The hidden library is rumored to have great treasures!'
+  ]);
+
+};
