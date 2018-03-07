486caf26a1825117d4501f811adefed92c0e49a9
Kyle Kemp <kyle@seiyria.com>
2018-03-07 00:39:43 -0600

    closes #546

--- src/server/effects/misc/Push.ts
+++ src/server/effects/misc/Push.ts
@@ -30,9 +30,9 @@ export class Push extends SpellEffect {
     const baseStat = caster.getTotalStat(userStat);
     const targetStat = target.getTotalStat(resistStat);
 
-    const successChance = clamp(baseStat - targetStat + 4, 0, 8) * 12.5;
+    const successChance = clamp((baseStat - targetStat) + 4, 0, 8) * 12.5;
 
-    if(random(0, 100) < successChance) {
+    if(random(0, 100) > successChance) {
       if(!predetermined) caster.sendClientMessage(`${target.name} resisted your push!`);
       return;
     }
