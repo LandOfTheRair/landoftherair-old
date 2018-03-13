507e2367569e48caaa9d6fc926377d04ab146a96
Kyle Kemp <kyle@seiyria.com>
2018-03-13 16:47:19 -0500

    logic fix for hostility checking with disguised enemies

--- src/shared/models/gamestate.ts
+++ src/shared/models/gamestate.ts
@@ -355,7 +355,7 @@ export class GameState {
   private checkTargetForHostility(me: NPC, target: Character): boolean {
     if(target.allegiance === 'NaturalResource' || target.allegiance === 'GM') return false;
     if(me.agro[target.uuid] || target.agro[me.uuid]) return true;
-    if(target.hasEffect('Disguise')) return me.getTotalStat('cha') > target.getTotalStat('wil');
+    if(target.hasEffect('Disguise') && me.getTotalStat('wil') < target.getTotalStat('cha')) return false;
     if(me.hostility === 'Faction' && (
          me.isHostileTo(target.allegiance)
       || target.isHostileTo(me.allegiance))) return true;
