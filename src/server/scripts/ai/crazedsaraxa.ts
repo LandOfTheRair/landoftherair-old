dd7cc1e9e1033ac31ed9a2db230d0a3a3ed668d6
Kyle Kemp <kyle@seiyria.com>
2018-04-04 08:48:31 -0500

    adjust how csaraxa stores acolytes

--- src/server/scripts/ai/crazedsaraxa.ts
+++ src/server/scripts/ai/crazedsaraxa.ts
@@ -1,42 +1,11 @@
 
-import { filter } from 'lodash';
+import { filter, compact } from 'lodash';
+import { nonenumerable } from 'nonenumerable';
 
 import { Character } from '../../../shared/models/character';
 import { NPC } from '../../../shared/models/npc';
 import { DefaultAIBehavior } from './default';
 import { SpellEffect } from '../../base/Effect';
-
-const acolytes: NPC[] = [];
-
-class AcolyteOverseer extends SpellEffect {
-  iconData = {
-    name: 'ages',
-    bgColor: '#a06',
-    color: '#fff',
-    tooltipDesc: 'Receiving 4% healing per acolyte every 5 seconds.'
-  };
-
-  private ticks = 0;
-
-  cast(target: NPC) {
-    this.duration = 500;
-    target.applyEffect(this);
-  }
-
-  effectTick(char: Character) {
-    this.ticks++;
-
-    const livingAcolytes = filter(acolytes, (ac: Character) => ac && !ac.isDead());
-    if(livingAcolytes.length > 0) {
-      if(this.ticks % 5 !== 0) return;
-      char.hp.add(char.hp.maximum * 0.04 * livingAcolytes.length);
-      return;
-    }
-
-    this.effectEnd(char);
-    char.unapplyEffect(this);
-  }
-}
 
 export class CrazedSaraxaAIBehavior extends DefaultAIBehavior {
 
@@ -44,10 +13,16 @@ export class CrazedSaraxaAIBehavior extends DefaultAIBehavior {
   private trigger50 = false;
   private trigger25 = false;
 
+  private acolytes: NPC[] = [];
+
+  public get livingAcolytes(): NPC[] {
+    return compact(filter(this.acolytes, (ac: NPC) => ac && !ac.isDead()));
+  }
+
   private async spawnAcolyte(spawnId: number) {
     const npc = this.npc;
 
-    if(acolytes[spawnId] && !acolytes[spawnId].isDead()) return;
+    if(this.acolytes[spawnId] && !this.acolytes[spawnId].isDead()) return;
 
     const msgObject = { name: npc.name, message: `Come forth, my acolyte!`, subClass: 'chatter' };
     npc.sendClientMessageToRadius(msgObject, 10);
@@ -62,13 +37,13 @@ export class CrazedSaraxaAIBehavior extends DefaultAIBehavior {
 
     if(!npc.hasEffect('AcolyteOverseer')) {
       const buff = new AcolyteOverseer({});
-      buff.cast(npc);
+      buff.cast(npc, npc, null, this);
     }
 
-    acolytes[spawnId] = acolyte;
+    this.acolytes[spawnId] = acolyte;
 
     acolyte.$$ai.death.add(() => {
-      acolytes[spawnId] = null;
+      this.acolytes[spawnId] = null;
     });
 
     const acolyteMessageObject = { name: npc.name, message: `The acolyte begins channeling energy back to Crazed Saraxa!`, subClass: 'environment' };
@@ -111,5 +86,39 @@ export class CrazedSaraxaAIBehavior extends DefaultAIBehavior {
     npc.sendClientMessageToRadius('You hear a lock click in the distance.', 50);
 
     npc.$$room.state.getInteractableByName('Chest Door').properties.requireLockpick = false;
+  }
+}
+
+class AcolyteOverseer extends SpellEffect {
+  iconData = {
+    name: 'ages',
+    bgColor: '#a06',
+    color: '#fff',
+    tooltipDesc: 'Receiving 4% healing per acolyte every 5 seconds.'
+  };
+
+  private ticks = 0;
+
+  @nonenumerable
+  private aiRef: CrazedSaraxaAIBehavior;
+
+  cast(caster: NPC, target: NPC, skillRef?, ai?: CrazedSaraxaAIBehavior) {
+    this.duration = 500;
+    this.aiRef = ai;
+    target.applyEffect(this);
+  }
+
+  effectTick(char: Character) {
+    this.ticks++;
+
+    const numLivingAcolytes = this.aiRef.livingAcolytes.length;
+    if(numLivingAcolytes > 0) {
+      if(this.ticks % 5 !== 0) return;
+      char.hp.add(char.hp.maximum * 0.04 * numLivingAcolytes);
+      return;
+    }
+
+    this.effectEnd(char);
+    char.unapplyEffect(this);
   }
 }
