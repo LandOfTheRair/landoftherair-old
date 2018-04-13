0f57a4c97e27e034bab16cd31114abff302a5ae7
Kyle Kemp <kyle@seiyria.com>
2018-04-13 15:45:12 -0500

    Update autogenerated files.

--- src/shared/generated/skilltrees/index.ts
+++ src/shared/generated/skilltrees/index.ts
@@ -1,8 +1,12 @@
 
 import * as MageTree from './Mage.json';
+import * as ThiefTree from './Thief.json';
 import * as HealerTree from './Healer.json';
+import * as WarriorTree from './Warrior.json';
 
 export const AllTrees = {
   Mage: MageTree,
-  Healer: HealerTree
+  Thief: ThiefTree,
+  Healer: HealerTree,
+  Warrior: WarriorTree
 };
