7ca30c94c521ff49c68af93b5e7a4b0ada37fa07
Kyle Kemp <kyle@seiyria.com>
2018-04-15 14:04:28 -0500

    make thief trainer compile

--- src/server/scripts/npc/trainer/thief.ts
+++ src/server/scripts/npc/trainer/thief.ts
@@ -3,21 +3,6 @@ import { NPCLoader } from '../../../helpers/character/npc-loader';
 import { BaseClassTrainerResponses } from '../common-responses';
 import { SkillClassNames } from '../../../../shared/models/character';
 
-const learnedSkills = { Thievery: {
-  2: ['Identify'],
-  3: ['TrueSight'],
-  5: ['Poison'],
-  7: ['Transmute'],
-  9: ['Disease'],
-  10: ['Darkness'],
-  12: ['Debilitate'],
-  13: ['DarkVision'],
-  14: ['EagleEye'],
-  15: ['ShadowMeld'],
-  16: ['Disguise'],
-  20: ['ShadowClones']
-} };
-
 export const setup = async (npc: NPC) => {
   npc.hostility = 'Never';
 
@@ -29,5 +14,5 @@ export const setup = async (npc: NPC) => {
 };
 
 export const responses = (npc: NPC) => {
-  BaseClassTrainerResponses(npc, learnedSkills);
+  BaseClassTrainerResponses(npc);
 };
