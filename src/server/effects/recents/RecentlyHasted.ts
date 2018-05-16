15928ea738acbcd8288aa2976938d41be3427ac1
Kyle Kemp <kyle@seiyria.com>
2018-05-16 13:57:24 -0500

    haste is 33% slow for the recently effect

--- src/server/effects/recents/RecentlyHasted.ts
+++ src/server/effects/recents/RecentlyHasted.ts
@@ -8,11 +8,19 @@ export class RecentlyHasted extends SpellEffect {
   iconData = {
     name: 'time-trap',
     color: '#000',
-    tooltipDesc: 'Recently hasted. Hasting during this period will lower one physical stat.'
+    tooltipDesc: 'Recently hasted. Actions are 33% slower. Hasting during this period will lower one physical stat.'
   };
+
+  private ticks: number;
 
   cast(caster: Character, target: Character, skillRef?: Skill) {
     this.duration = 60;
+    this.ticks = 0;
     target.applyEffect(this);
+  }
+
+  effectTick(char: Character) {
+    this.ticks++;
+    this.effectInfo.isFrozen = this.ticks % 3 === 0;
   }
 }
