fd1f61ace6d7d5a70a48b3662ce826f2211ef5e9
Kyle Kemp <kyle@seiyria.com>
2018-05-22 06:43:37 -0500

    create maps in non cluster too

--- src/server/index.ts
+++ src/server/index.ts
@@ -107,6 +107,10 @@ if(process.argv[2] === '--single-core') {
         allMapNames[mapName] = true;
         const proto = includes(mapName, '-Dungeon') ? Rooms.InstancedDungeon : Rooms.GameWorld;
         gameServer.register(mapName, proto, { mapName, mapPath: file, allMapNames });
+
+        if(!includes(mapName, '-Dungeon')) {
+          gameServer.matchMaker.create(mapName, {});
+        }
       });
 
       if(process.env.NODE_ENV !== 'production') {
