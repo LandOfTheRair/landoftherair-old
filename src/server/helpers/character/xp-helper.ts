8429d9de4d0ec568d3fe2fffc74c514b8c71ea04
Kyle Kemp <kyle@seiyria.com>
2018-05-24 19:58:20 -0500

    add xp for post-50 so I don't forget what I want to do later

--- src/server/helpers/character/xp-helper.ts
+++ src/server/helpers/character/xp-helper.ts
@@ -14,6 +14,14 @@ export class XPHelper {
       return pre20XP * (Math.max(1, level - FIRST_LEVEL_CONSTANT_CHANGER));
     }
 
+    const level50XP = pre20XP * (Math.max(1, 50 - FIRST_LEVEL_CONSTANT_CHANGER));
+
+    if(level === 51) return Math.floor(level50XP * 1.5);
+    if(level === 52) return Math.floor(level50XP * 3);
+    if(level === 53) return Math.floor(level50XP * 5);
+    if(level === 54) return Math.floor(level50XP * 7.5);
+    if(level === 55) return Math.floor(level50XP * 10.5);
+
     return 99999999999999999999999 * level;
   }
 }
