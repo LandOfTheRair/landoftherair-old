6813d8391de208bd7b3cf8c886222cd601aa3d1a
Kyle Kemp <kyle@seiyria.com>
2018-09-15 21:05:49 -0500

    Revert "use redis presence, update colyseus"
    This reverts commit 61877dfa1c451ccb44f89e009164b9dd0d96d798.

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,7 +8,7 @@ import { Logger } from './logger';
 
 import { includes } from 'lodash';
 
-import { Server, RedisPresence } from 'colyseus';
+import { Server, MemsharedPresence } from 'colyseus';
 import { monitor } from '@colyseus/monitor';
 
 import * as Rooms from './rooms';
@@ -85,9 +85,7 @@ DB.init()
         const server = http.createServer(api.expressApp);
 
         const gameServer = new Server({
-          presence: new RedisPresence({
-            url: process.env.REDIS_URL
-          }),
+          presence: new MemsharedPresence(),
           server
         });
 
