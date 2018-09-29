ef5c58a529fc8ae17e4936ed777a55881f000845
Kyle Kemp <kyle@seiyria.com>
2018-09-28 23:03:03 -0500

    one more title change

--- src/server/scripts/npc/tutorial/hermit.ts
+++ src/server/scripts/npc/tutorial/hermit.ts
@@ -2,7 +2,7 @@ import { NPC } from '../../../../shared/models/npc';
 
 export const setup = async (npc: NPC) => {
   npc.hostility = 'Never';
-  npc.affiliation = 'Quest Giver';
+  npc.affiliation = 'Drakkarian Hermit';
 
   npc.leftHand = await npc.$$room.npcLoader.loadItem('Tutorial Yeti Skull');
   npc.rightHand = await npc.$$room.npcLoader.loadItem('Tutorial Key');
