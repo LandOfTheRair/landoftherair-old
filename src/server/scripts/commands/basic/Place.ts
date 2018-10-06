61ad319b60854db61f0a8bafbb881dc77bfe7785
Kyle Kemp <kyle@seiyria.com>
2018-10-06 14:46:55 -0500

    closes #1032

---
+++ src/server/scripts/commands/basic/Place.ts
@@ -0,0 +1,45 @@
+
+import { includes } from 'lodash';
+
+import { Command } from '../../../base/Command';
+import { Player } from '../../../../shared/models/player';
+
+export class Place extends Command {
+
+  public name = 'place';
+  public format = 'ItemType|ItemName [in sack|belt|pouch]';
+
+  execute(player: Player, { args }) {
+    if(this.isAccessingLocker(player)) return;
+
+    if(includes(args, ' in')) args = args.split(' in').join('');
+
+    let [itemTypeOrName, container] = args.split(' ');
+
+    itemTypeOrName = itemTypeOrName.toLowerCase();
+    container = (container || 'sack').toLowerCase();
+
+    if(!includes(['belt', 'sack', 'pouch'], container)) return player.sendClientMessage('Invalid container.');
+
+    let item = null;
+    let hand = null;
+
+    if(player.rightHand
+    && (player.rightHand.itemClass.toLowerCase() === itemTypeOrName
+    || includes(player.rightHand.name.toLowerCase(), itemTypeOrName))) { hand = 'right'; item = player.rightHand; }
+
+    if(player.leftHand
+    && (player.leftHand.itemClass.toLowerCase() === itemTypeOrName
+    || includes(player.leftHand.name.toLowerCase(), itemTypeOrName))) { hand = 'left'; item = player.leftHand; }
+
+    if(!item) return false;
+
+    if(container === 'sack' && !player.addItemToSack(item)) return;
+    if(container === 'belt' && !player.addItemToBelt(item)) return;
+    if(container === 'pouch' && !player.addItemToPouch(item)) return;
+
+    if(hand === 'right') player.setRightHand(null);
+    if(hand === 'left') player.setLeftHand(null);
+  }
+
+}
---
+++ src/server/scripts/commands/basic/Take.ts
@@ -0,0 +1,45 @@
+
+import { includes } from 'lodash';
+
+import { Command } from '../../../base/Command';
+import { Player } from '../../../../shared/models/player';
+
+export class Take extends Command {
+
+  public name = 'take';
+  public format = 'ItemType|ItemName [from sack|belt|pouch]';
+
+  execute(player: Player, { args }) {
+    if(this.isAccessingLocker(player)) return;
+    if(!player.hasEmptyHand()) return player.sendClientMessage('Your hands are full.');
+
+    if(includes(args, ' from')) args = args.split(' from').join('');
+
+    let [itemTypeOrName, container] = args.split(' ');
+
+    itemTypeOrName = itemTypeOrName.toLowerCase();
+    container = (container || 'sack').toLowerCase();
+
+    if(!includes(['belt', 'sack', 'pouch'], container)) return player.sendClientMessage('Invalid container.');
+
+    const containerItems = player[container].allItems;
+    let takeItemSlot = -1;
+
+    containerItems.forEach((checkItem, i) => {
+      if(takeItemSlot >= 0) return;
+
+      if(checkItem.itemClass.toLowerCase() === itemTypeOrName) takeItemSlot = i;
+      if(includes(checkItem.name.toLowerCase(), itemTypeOrName)) takeItemSlot = i;
+    });
+
+    if(takeItemSlot === -1) return player.sendClientMessage('Item was not found.');
+
+    const item = player[container].takeItemFromSlot(takeItemSlot);
+    if(!item) return false;
+
+    if(!player.rightHand) player.setRightHand(item);
+    else if(!player.leftHand) player.setLeftHand(item);
+
+  }
+
+}
