afd29b890652f0c6e7f6e94dccb49406e17662ff
Kyle Kemp <kyle@seiyria.com>
2018-10-27 17:01:54 -0500

    remove poison effect from chill bite

--- src/server/scripts/commands/skills/monster/ChillBiteMedium.ts
+++ src/server/scripts/commands/skills/monster/ChillBiteMedium.ts
@@ -1,7 +1,6 @@
 
 import { MonsterSkill } from '../../../../base/Skill';
 import { Character } from '../../../../../shared/models/character';
-import { Poison as CastEffect } from '../../../../effects/dots/Poison';
 import { CombatHelper } from '../../../../helpers/world/combat-helper';
 import { RollerHelper } from '../../../../../shared/helpers/roller-helper';
 
@@ -21,8 +20,6 @@ export class ChillBiteMedium extends MonsterSkill {
       attackerDamageMessage: '',
       defenderDamageMessage: `%0 sunk cold fangs into you!`
     });
-    const effect = new CastEffect({ potency: 4, duration: 10 });
-    effect.cast(user, target, this);
   }
 
 }
--- src/server/scripts/commands/skills/monster/ChillBiteStrong.ts
+++ src/server/scripts/commands/skills/monster/ChillBiteStrong.ts
@@ -1,9 +1,6 @@
 
-import * as dice from 'dice.js';
-
 import { MonsterSkill } from '../../../../base/Skill';
 import { Character } from '../../../../../shared/models/character';
-import { Poison as CastEffect } from '../../../../effects/dots/Poison';
 import { CombatHelper } from '../../../../helpers/world/combat-helper';
 import { RollerHelper } from '../../../../../shared/helpers/roller-helper';
 
@@ -23,8 +20,6 @@ export class ChillBiteStrong extends MonsterSkill {
       attackerDamageMessage: '',
       defenderDamageMessage: `%0 sunk cold fangs into you!`
     });
-    const effect = new CastEffect({ potency: 6, duration: 15 });
-    effect.cast(user, target, this);
   }
 
 }
