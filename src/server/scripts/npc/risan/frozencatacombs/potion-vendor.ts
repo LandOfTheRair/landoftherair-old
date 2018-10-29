b4865b73d37623c0456e1ea780d45b2de4296d91
Kyle Kemp <kyle@seiyria.com>
2018-10-29 10:37:04 -0500

    add potion vendor to catacombs, #1109

---
+++ src/server/scripts/npc/risan/frozencatacombs/potion-vendor.ts
@@ -0,0 +1,25 @@
+import { NPC } from '../../../../../shared/models/npc';
+import { VendorResponses } from '../../common-responses';
+
+export const setup = async (npc: NPC) => {
+  npc.hostility = 'Never';
+  npc.affiliation = 'Potion Vendor';
+
+  const vendorItems = [
+    'Mend Bottle',
+    'Mend Bottle (5oz)',
+    'Instant Heal Bottle',
+    'Instant Heal Bottle (5oz)',
+    { name: 'ScaleShredder Potion', valueMult: 25 },
+  ];
+
+  npc.$$room.npcLoader.loadVendorItems(npc, vendorItems);
+
+  npc.rightHand = await npc.$$room.npcLoader.loadItem('Instant Heal Bottle');
+  npc.gear.Armor = await npc.$$room.npcLoader.loadItem('Antanian Tunic');
+  npc.recalculateStats();
+};
+
+export const responses = (npc: NPC) => {
+  VendorResponses(npc);
+};
