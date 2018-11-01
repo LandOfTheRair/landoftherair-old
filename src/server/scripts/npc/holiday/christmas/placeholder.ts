257d4139e91c3d022846e071e11f6270d24853a9
seiyria <kyle@seiyria.com>
2018-10-31 21:15:07 -0500

    add basic christmas map

---
+++ src/server/scripts/npc/holiday/christmas/placeholder.ts
@@ -0,0 +1,21 @@
+import { NPC } from '../../../../../shared/models/npc';
+
+export const setup = async (npc: NPC) => {
+  npc.hostility = 'Never';
+  npc.affiliation = 'Christmas Helpers';
+
+  npc.gear.Armor = await npc.$$room.npcLoader.loadItem('Risan Tunic');
+
+  npc.recalculateStats();
+};
+
+export const responses = (npc: NPC) => {
+
+  npc.parser.addCommand('hello')
+    .set('syntax', ['hello'])
+    .set('logic', (args, { player }) => {
+      if(npc.distFrom(player) > 0) return 'Please move closer.';
+
+      return `Hello!`;
+    });
+};
