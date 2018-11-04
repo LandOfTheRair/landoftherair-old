940e5f2c50566bb8081dd1991fe9c0e68ccf88d3
Kyle Kemp <kyle@seiyria.com>
2018-11-04 10:33:29 -0600

    closes #1261

---
+++ src/server/scripts/npc/trainer/tradeskill.ts
@@ -0,0 +1,17 @@
+import { NPC } from '../../../../shared/models/npc';
+import { AutoRevives, BaseClassTrainerResponses, RecallerResponses, ReviverResponses } from '../common-responses';
+import { SkillClassNames } from '../../../../shared/models/character';
+
+export const setup = async (npc: NPC) => {
+  npc.hostility = 'Never';
+  npc.affiliation = 'Master of Tradeskills';
+
+  npc.gear.Armor = await npc.$$room.npcLoader.loadItem('Antanian Tunic');
+  npc.classTrain = 'Undecided';
+  npc.trainSkills = [SkillClassNames.Alchemy, SkillClassNames.Spellforging, SkillClassNames.Runewriting, SkillClassNames.Metalworking, SkillClassNames.Survival];
+  npc.recalculateStats();
+};
+
+export const responses = (npc: NPC) => {
+  BaseClassTrainerResponses(npc);
+};
