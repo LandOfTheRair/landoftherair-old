55ef6147b59945b5d7e82ed9f5de1281abf26218
seiyria <kyle@seiyria.com>
2018-12-01 10:52:56 -0600

    closes #1372

--- src/client/app/_shared/character-tagline.component.ts
+++ src/client/app/_shared/character-tagline.component.ts
@@ -4,10 +4,16 @@ import { IPlayer } from '../../../shared/interfaces/character';
 @Component({
   selector: 'app-character-tagline',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `<span *ngIf="player">{{ player.name }} the {{ player.alignment }} level {{ player.level }} {{ player.baseClass }}</span>`
+  template: `
+  <span *ngIf="player">
+    {{ player.name }}<span *ngIf="!short"> the {{ player.alignment }} level {{ player.level }} {{ player.baseClass }}</span>
+  </span>`
 })
 export class PlayerTaglineComponent {
 
   @Input()
   public player: IPlayer;
+
+  @Input()
+  public short: boolean;
 }
