d15dd90d31d1fe8f2f96fa2239638744d5dfcce4
Kyle Kemp <kyle@seiyria.com>
2018-12-13 08:00:46 -0600

    closes #1395

--- src/client/app/journal/journal.component.ts
+++ src/client/app/journal/journal.component.ts
@@ -1,24 +1,14 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { LocalStorageService } from 'ngx-webstorage';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LocalStorage } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-journal',
   templateUrl: './journal.component.html',
   styleUrls: ['./journal.component.scss']
 })
-export class JournalComponent implements OnInit {
+export class JournalComponent {
 
-  @ViewChild('journal')
-  public journalEl: ElementRef;
-
-  constructor(private localStorage: LocalStorageService) { }
-
-  async ngOnInit() {
-    this.journalEl.nativeElement.innerText = await this.localStorage.retrieve('journal') || 'Click to edit the contents of your journal.';
-  }
-
-  save() {
-    this.localStorage.store('journal', this.journalEl.nativeElement.innerText);
-  }
+  @LocalStorage('journal')
+  public journal: string;
 
 }
