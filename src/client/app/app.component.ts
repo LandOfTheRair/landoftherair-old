1957920aaa96956da931f22e9ba7e99911cd5c7a
Kyle Kemp <kyle@seiyria.com>
2018-12-28 10:27:19 -0600

    catch error

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -670,7 +670,7 @@ export class AppComponent implements OnInit {
       }
     }).then((handler: StripeCheckoutHandler) => {
       this.stripeCheckoutHandler = handler;
-    });
+    }).catch(() => {});
   }
 
   startPayment(item) {
