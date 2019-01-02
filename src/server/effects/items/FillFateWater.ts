fdcd3221c412f05b3f7d5b7e6e2cf0ddc3b448ed
Kyle Kemp <kyle@seiyria.com>
2019-01-02 14:13:18 -0600

    fix build

--- src/server/effects/items/FillFateWater.ts
+++ src/server/effects/items/FillFateWater.ts
@@ -1,10 +1,10 @@
 
 import { Effect } from '../../base/Effect';
-import { Character } from '../../../shared/models/character';
 import { Fate } from '../misc/Fate';
+import { Player } from '../../../shared/models/player';
 
 export class FillFateWater extends Effect {
-  effectStart(char: Character) {
+  effectStart(char: Player) {
     const fate = new Fate({});
     fate.cast(char, char);
   }
